5d852ed38d7f4c06808002c07219a256fa2bfa52
unknown <sakyabdi1@gmail.com>
2024-03-23 19:51:18 -0400

    Update: Statsenal v0.1.0
    Update:
    Statsenal now allows for the ability to collect statistics by the respective season chosen by the user.
    
    Patches:
    - Added missing "time" module to code.
    
    - Fixed an error that corrupted generated image files.
    
    - Fixed an error that causes "club_choice" to refresh to the default.
    
    - Fixed a labeling issue with generated graphs.
    
    - Adjusted naming conventions for variables.
    
    - Removed redundant code.

--- Statsenal GUI.py
+++ Statsenal GUI.py
@@ -1,6 +1,7 @@
 import customtkinter
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 import pandas as pd
 from datetime import date
 import os
@@ -15,7 +16,7 @@ root = customtkinter.CTk()
 
 root.geometry("500x350")
 
-root.title('Statsenal v0.01')
+root.title('Statsenal v0.1.0')
 
 root.resizable(0,0)
 
@@ -25,9 +26,10 @@ label.grid(row=1, pady=12, padx=100)
 def combo_selection():
     club_choice = club_options.get()
     stat_choice = stat_options.get()
-    Club_picker(club_choice, stat_choice)
+    season_choice = season_options.get()
+    club_picker(club_choice, stat_choice, season_choice)
     
-def Club_picker(club_choice, stat_choice):
+def club_picker(club_choice, stat_choice, season_choice):
     driver = webdriver.Chrome()
     driver.maximize_window()
     if stat_choice=='Goals':
@@ -39,9 +41,9 @@ def Club_picker(club_choice, stat_choice):
     else:
         pass
 
-    Cookies_button = driver.find_element(By.ID, "onetrust-accept-btn-handler")    
-    if Cookies_button == driver.find_element(By.ID, "onetrust-accept-btn-handler"):
-        Cookies_button.click()
+    cookies_button = driver.find_element(By.ID, "onetrust-accept-btn-handler")    
+    if cookies_button == driver.find_element(By.ID, "onetrust-accept-btn-handler"):
+        cookies_button.click()
     else:
         pass
     time.sleep(1)
@@ -50,16 +52,29 @@ def Club_picker(club_choice, stat_choice):
     driver.execute_script("arguments[0].click();", hidden_element)
     time.sleep(1)
 
-    Club_Filter_button= driver.find_element(By.XPATH, '//*[@id="mainContent"]/div[2]/div[1]/section/div[3]/div[2]')
-    Club_Filter_button.click()
-    time.sleep(1)
-
-    if club_choice==club_choice:
-        Arsenal_Filter_button= driver.find_element(By.XPATH, f'//li[@data-option-name="{club_choice}"]')
-        Arsenal_Filter_button.click()
+    if season_choice==season_choice:
+        driver.execute_script("arguments[0].click();", hidden_element)
+        time.sleep(1)
+        season_filter_button= driver.find_element(By.XPATH, '//*[@id="mainContent"]/div[2]/div[1]/section/div[2]/div[2]')
+        season_filter_button.click()
+        time.sleep(1)
+        season_selection_button= driver.find_element(By.XPATH, f'//li[@data-option-name="{season_choice}"]')
+        season_selection_button.click()
+        time.sleep(1)
     else:
         pass
-    time.sleep(1)
+    if club_choice==club_choice:
+        driver.execute_script("arguments[0].click();", hidden_element)
+        time.sleep(1)
+        club_filter_button= driver.find_element(By.XPATH, '//*[@id="mainContent"]/div[2]/div[1]/section/div[3]/div[2]')
+        club_filter_button.click()
+        time.sleep(1)
+        club_selection_button= driver.find_element(By.XPATH, f'//li[@data-option-name="{club_choice}"]')
+        club_selection_button.click()
+        time.sleep(1)
+    else:
+        pass
+        
     today = date.today()
     
     folder_path = 'Goals Spreadsheets'
@@ -68,50 +83,58 @@ def Club_picker(club_choice, stat_choice):
     else:
         pass
 
-    club_goal_link = driver.page_source.encode('ISO-8859-1')
-    Club_goal_data = pd.read_html(club_goal_link, header =0)[0]
-    Club_goal_date = f'{folder_path}/{club_choice}_{stat_choice}_List_{today}.csv'
-    Club_goal_data.to_csv(Club_goal_date, encoding='ISO-8859-1')
-    cgwb = pd.read_csv(Club_goal_date, encoding='ISO-8859-1')
+    stat_link = driver.page_source.encode('ISO-8859-1')
+    stat_data = pd.read_html(stat_link, header =0)[0]
+    stat_date = f'{folder_path}/{club_choice}_{stat_choice}_List_{today}.csv'
+    stat_data.to_csv(stat_date, encoding='ISO-8859-1')
+    stat_workbook = pd.read_csv(stat_date, encoding='ISO-8859-1')
     columns_to_drop = [0, 6]
-    cgwb = cgwb.drop(cgwb.columns[columns_to_drop], axis=1)
-    cgwb.to_csv(Club_goal_date, encoding='ISO-8859-1', index = False)
+    stat_workbook = stat_workbook.drop(stat_workbook.columns[columns_to_drop], axis=1)
+    stat_workbook.to_csv(stat_date, encoding='ISO-8859-1', index = False)
 
-    cgwb = pd.read_csv(Club_goal_date, encoding='ISO-8859-1')
-    x_axis = cgwb['Rank']
-    y_axis = cgwb['Stat']
-    names = cgwb['Player']
+    x_axis = stat_workbook['Rank']
+    y_axis = stat_workbook['Stat']
+    names = stat_workbook['Player']
     fig, ax = plt.subplots()
     plt.bar(x_axis, y_axis, label = "Player")
     player_names = []
     for i, txt in enumerate(names):
         player_names.append(ax.annotate(txt, (x_axis[i], y_axis[i])))
     adjust_text(player_names, expand = (1.0, 3.5), arrowprops = {'color': 'blue'})
-    plt.xlabel('Goals Scored') 
-    plt.ylabel('Rank') 
-    plt.title(f'{club_choice} {stat_choice} Stats:')
+    plt.xlabel('Rank') 
+    plt.ylabel('Goals Scored') 
+    plt.title(f'{club_choice} {stat_choice} Stats ({season_choice}):')
     plt.gcf().patch.set_edgecolor('b')
     plt.gcf().patch.set_linewidth(3)
-    plt.show()         
-    plt.savefig(f'{folder_path}/{club_choice} {stat_choice} Stats:'.png)
-    
-Premier_League_List = ["All Clubs", "Arsenal", "Aston Villa", "Bournemouth", "Brentford", "Brighton & Hove Albion", "Burnley", "Chelsea", "Crystal Palace", "Everton", "Fulham", "Liverpool", "Luton Town", "Manchester City", "Manchester United", "Newcastle United", "Nottingham Forest", "Sheffield United", "Tottenham Hotspur", "West Ham United", "Wolverhampton Wanderers"]
+    new_season_choice = season_choice.replace('/', '-')
+    plt.savefig(f'{folder_path}/{club_choice} {stat_choice} Stats for {new_season_choice}.png')
+    plt.show()
 
-Stat_List = ["Goals", "Assists"]
+premier_league_list = ["All Clubs", "Arsenal", "Aston Villa", "Bournemouth", "Brentford", "Brighton & Hove Albion", "Burnley", "Chelsea", "Crystal Palace", "Everton", "Fulham", "Liverpool", "Luton Town", "Manchester City", "Manchester United", "Newcastle United", "Nottingham Forest", "Sheffield United", "Tottenham Hotspur", "West Ham United", "Wolverhampton Wanderers"]
+
+stat_list = ["Goals", "Assists"]
+
+season_list = ["1992/93", "1993/94", "1994/95", "1995/96", "1996/97", "1997/98", "1998/99", "1999/00", "2000/01", "2001/02", "2002/03", "2003/04", "2004/05", "2005/06", "2006/07", "2007/08", "2008/09", "2009/10", "2010/11", "2011/12", "2012/13", "2013/14", "2014/15", "2015/16", "2016/17", "2017/18", "2018/19", "2019/20", "2020/21", "2021/22", "2022/23", "2023/24"]
 
 club_label = customtkinter.CTkLabel(root, text="Select a Club:", font=("Roboto", 20))
 club_label.grid(sticky='w', row= 2, pady=10, padx=10)
 
-club_options = customtkinter.CTkComboBox(root, values=Premier_League_List)
-club_options.grid(row=2, pady=10, padx=10)
+club_options = customtkinter.CTkComboBox(root, values=premier_league_list)
+club_options.grid(row=2, pady=10, padx=(50, 0))
 
-Stat_label = customtkinter.CTkLabel(root, text="Select a Stat:", font=("Roboto", 20))
-Stat_label.grid(sticky='w', row= 3, pady=0, padx=10)
+stat_label = customtkinter.CTkLabel(root, text="Select a Stat:", font=("Roboto", 20))
+stat_label.grid(sticky='w', row= 3, pady=0, padx=10)
 
-stat_options = customtkinter.CTkComboBox(root, values=Stat_List)
-stat_options.grid(row= 3, pady=12, padx=10)
+stat_options = customtkinter.CTkComboBox(root, values=stat_list)
+stat_options.grid(row= 3, pady=12, padx=(50, 0))
+
+season_label = customtkinter.CTkLabel(root, text="Select a Season:", font=("Roboto", 20))
+season_label.grid(sticky='w', row= 4, pady=0, padx=10)
+
+season_options = customtkinter.CTkComboBox(root, values=season_list)
+season_options.grid(row= 4, pady=12, padx=(50, 0))
 
 button = customtkinter.CTkButton(root, text="Select", command=combo_selection)
-button.grid(row=4, pady=12, padx=10)
+button.grid(row=5, pady=12, padx=(50, 0))
 
 root.mainloop()
